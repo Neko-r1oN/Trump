2e36fefa7df39e740c8e744af5655e0a9df418df
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-06-21 11:22:47 +0900

    ポーカー基盤作成

---
+++ TrumpPair/Card.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TrumpPair
+{
+    class Card
+    {
+        private bool loop = true;
+        private int CardNum = 0;
+        private int PairNum = 0;    //ペア合致数保存変数
+
+
+
+        /// <summary>
+        /// ステータス入力処理
+        /// </summary>
+        public void CreateCard()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+
+                while (loop == true)
+                {
+                    Console.Write("\n", i, "番目のカードの数値を入力＞ ");  // 文字列を表示
+
+                    if (int.TryParse(Console.ReadLine(), out CardNum))
+                    {
+                        if (CardNum > 4 || CardNum <= 0)
+                        {
+                            Console.WriteLine("範囲外の数字が入力されました。やり直してください。");
+
+                        }
+                        else
+                        {
+                            loop = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("数字ではない文字列が入力されました。やり直してください。");
+
+
+                    }
+                }
+
+            }
+
+        }
+        public void ShowCard()
+        {
+
+            Console.WriteLine("数字：{0}\n", CardNum);
+        }
+        public void CheckCard(int Card1, int Card2, int Card3, int Card4)
+        {
+            var s = new int[4];
+            var num = new int[4];
+            foreach (var e in s)
+                num[e - 1]++;
+
+            var max = 0;
+            var cnt2 = 0;
+            foreach (var e in num)
+            {
+                if (e > max) max = e;
+                if (e == 2) cnt2++;
+            }
+
+            switch (max)
+            {
+                case 4: Console.WriteLine("Four Card"); break;
+                case 3: Console.WriteLine(cnt2 == 1 ? "Full House" : "Three Card"); break;
+                case 2: Console.WriteLine(cnt2 == 2 ? "Two Pair" : "One Pair"); break;
+                default: Console.WriteLine("No hands"); break;
+            }
+        }
+
+    }
+
+}
---
+++ TrumpPair/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrumpPair
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="パーティ情報"></param>
+        static void Main(string[] args)
+        {
+            ///ループ用変数          
+            bool loop = true;
+            int I = 0;
+            int i = 0;
+            ///カード枚数用変数           
+            int CardNum = 4;
+
+            int number = 0;
+
+            int pair = 0;
+
+            // 配列を作成
+            Card[] cards = new Card[CardNum];
+
+            //ペア保存用変数
+            var num = new int[4];
+
+
+
+            // インスタンス生成
+            for (i = 0; i < CardNum; i++)
+            {// プレイヤー人数分ループ
+                cards[i] = new Card();
+            }
+
+            // ステータス入力
+            for (i = 0; i < CardNum; i++)
+            {
+                loop = true;
+                while (loop == true)
+                {
+                    Console.Write(i + 1);
+                    Console.Write("枚目のカードの数字を入力 >");
+
+
+               
+                                
+
+                    if (int.TryParse(Console.ReadLine(), out number))
+                    {
+                        if (number > 4 || number <= 0)
+                        {
+                            Console.WriteLine("範囲外の数字が入力されました。やり直してください。");
+                        }
+                        else
+                        {
+                            num[i] = number;
+                           loop = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("数字ではない文字列が入力されました。やり直してください。");
+                    }
+               }
+            }
+
+            ///プレイヤー情報出力
+            Console.WriteLine("\nカード情報\n\n ");  // 文字列を表示
+            int Inum = 3;
+
+            for (i = 0; i < CardNum; i++)
+            {
+                //カード枚数分ループ
+                Console.Write("{0}枚目 : ", i + 1);  // 文字列を表示
+
+                Console.Write("{0}\n", num[i]);  // 文字列を表示
+
+                for (I = 0; I < Inum; I++)
+                {
+
+                    //Console.Write("{0}\n", num[i]);  // 文字列を表示
+                    //Console.Write("{0}\n", num[I + i + 1]);  // 文字列を表示
+
+                    //合致する数字があった場合
+                    if (num[i] == num[I + i + 1])
+                    {
+                        pair++;
+                    }
+                }
+                //ループ上限を下げる
+                Inum--;
+                
+            }
+            
+            if(CardNum < pair)
+            {
+                pair = CardNum;
+
+            }
+            Console.Write("ペア数 : {0}\n", pair);  // 文字列を表示
+
+
+
+
+            switch (pair)
+            {
+                case 4: Console.WriteLine("Four Card"); break;
+                case 3: Console.WriteLine("Three Card"); break;
+                case 2: Console.WriteLine("Two Pair"); break;
+                case 1: Console.WriteLine("One Pair"); break;
+                default: Console.WriteLine("No hands"); break;
+            }
+
+            Console.Write("\nエンターを押して閉じる");
+            Console.ReadLine();
+
+        }
+    }
+}
